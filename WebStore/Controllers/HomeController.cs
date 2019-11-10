fdd3eb9fdcac9ba942ffc51aca2fd175ad7a9403
Дмитрий Самсонов <NomekopSD@yandex.ru>
2019-11-10 16:05:50 +0300

    Add employee controller Transfer employee action to employee controller Fix links

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -27,39 +26,5 @@ namespace WebStore.Controllers
             return Content(_Configuration["CustomData"]);
         }
 
-        private static readonly List<EmployeeView> __Employees = new List<EmployeeView>
-        {
-            new EmployeeView {Id = 1, FirstName = "Иван", SecondName = "Иванов", Patronymic = "Иванович", Age = 40, 
-                Birthday = new DateTime(2000, 9, 11), StartWork = new DateTime(2020, 9, 11), Position = EmployeePosition.Manager},
-            new EmployeeView {Id = 2, FirstName = "Петр", SecondName = "Петров", Patronymic = "Петрович", Age = 30, 
-                Birthday = new DateTime(2010, 9, 11), StartWork = new DateTime(2030, 9, 11), Position = EmployeePosition.Seller},
-            new EmployeeView {Id = 3, FirstName = "Сидор", SecondName = "Сидоров", Patronymic = "Сидорович", Age = 20, 
-                Birthday = new DateTime(2020, 9, 11), StartWork = new DateTime(2040, 9, 11), Position = EmployeePosition.Probation}
-        };
-
-        public IActionResult GetEmployees()
-        {
-            ViewBag.SomeData = "Hello World!";
-            ViewData["Test"] = "TestData";
-
-            return View(__Employees);
-        }
-
-        public IActionResult EmployeeDetails(int id)
-        {
-            EmployeeView result = new EmployeeView
-            { Id = id };
-
-            foreach (var employee in __Employees)
-            {
-                if (employee.Id == id)
-                {
-                    result = employee;
-                    break;
-                }
-            }
-
-            return View(result);
-        }
     }
 }
