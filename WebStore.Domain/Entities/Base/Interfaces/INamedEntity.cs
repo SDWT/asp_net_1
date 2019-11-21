61319c0e4fa524a1233dee48dfdda8a4fc8c07d0
Дмитрий Самсонов <NomekopSD@yandex.ru>
2019-11-21 16:12:22 +0300

    Base Entites

---
+++ WebStore.Domain/Entities/Base/Interfaces/INamedEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace WebStore.Domain.Entities.Base.Interfaces
+{
+    /// <summary>Интерфейс именованой сущности</summary>
+    public interface INamedEntity : IBaseEntity
+    {
+        /// <summary>Имя</summary>
+        string Name { get; set; }
+    }
+}
