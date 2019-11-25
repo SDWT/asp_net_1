107919d32a05108b8d0fc4d9e2d05441abe72f82
Дмитрий Самсонов <NomekopSD@yandex.ru>
2019-11-25 22:54:17 +0300

    Доработка сущностей для их размещения в БД

--- WebStore.Domain/Entities/Product.cs
+++ WebStore.Domain/Entities/Product.cs
@@ -1,21 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
 {
+    //[Table("Products")]
     public class Product : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
 
         public int SectionId { get; set; }
 
+        [ForeignKey(nameof(SectionId))]
+        public virtual Section Section { get; set; }
+
         public int? BrandId { get; set; }
+
+        [ForeignKey(nameof(BrandId))]
+        public virtual Brand Brand { get; set; } // автоматически генерируемый внешний ключ - Brand_id
 
         public string ImageUrl { get; set; }
 
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        // Пример для создания поля с определённым названием колонки
+        //[Column(name:"TestValueColumn")]
+        //public string TestValue { get; set; }
+
+        // Пример для создания поля, которое игнорируется EntityFramework для создания колонки
+        //[NotMapped]
+        //public int NotMappedProperty { get; set; }
     }
 }
